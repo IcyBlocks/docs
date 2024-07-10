b8259892e9fe71a42d747b687a6630b12d1fb6ae
Icy <127612070+IcyBlocks@users.noreply.github.com>
2024-07-11 02:14:39 +0300

    Add files via upload

---
+++ theme.config.tsx
@@ -0,0 +1,85 @@
+import React from 'react';
+import { DocsThemeConfig } from 'nextra-theme-docs';
+
+const StoreIcon = () => (
+  <svg
+    width="24"
+    height="24"
+    viewBox="0 0 576 512"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      fill="currentColor"
+      d="M547.6 103.8L490.3 13.1C485.2 5 476.1 0 466.4 0H109.6C99.9 0 90.8 5 85.7 13.1L28.3 103.8c-29.6 46.8-3.4 111.9 51.9 119.4c4 .5 8.1 .8 12.1 .8c26.1 0 49.3-11.4 65.2-29c15.9 17.6 39.1 29 65.2 29c26.1 0 49.3-11.4 65.2-29c15.9 17.6 39.1 29 65.2 29c26.2 0 49.3-11.4 65.2-29c16 17.6 39.1 29 65.2 29c4.1 0 8.1-.3 12.1-.8c55.5-7.4 81.8-72.5 52.1-119.4zM499.7 254.9l-.1 0c-5.3 .7-10.7 1.1-16.2 1.1c-12.4 0-24.3-1.9-35.4-5.3V384H128V250.6c-11.2 3.5-23.2 5.4-35.6 5.4c-5.5 0-11-.4-16.3-1.1l-.1 0c-4.1-.6-8.1-1.3-12-2.3V384v64c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V384 252.6c-4 1-8 1.8-12.3 2.3z"
+    />
+  </svg>
+);
+
+const config: DocsThemeConfig = {
+  banner: {
+    key: 'discount',
+    text: (
+      <a href="https://store.cubic-rp.com" target="_blank">
+        🎉 Use code 'DOCS-15' at checkout for a 15% discount! Cubic Store →
+      </a>
+    )
+  },
+  logo: (
+    <>
+      <svg
+        width="24"
+        height="24"
+        viewBox="0 0 100 100"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          fill="#E0E0E0"
+          stroke="#4A4A4A"
+          strokeWidth="2"
+          d="M50 2 L98 25 L98 75 L50 98 L2 75 L2 25 Z"
+        />
+        <path
+          fill="#FFFFFF"
+          stroke="#4A4A4A"
+          strokeWidth="2"
+          d="M50 2 L2 25 L50 48 L98 25 Z"
+        />
+        <path
+          fill="none"
+          stroke="#4A4A4A"
+          strokeWidth="2"
+          d="M50 48 L50 98"
+        />
+      </svg>
+      <span style={{ marginLeft: '.4em', marginTop: '.1em', fontWeight: 900, fontFamily: "'Montserrat', sans-serif" }}>
+        Cubic Store
+      </span>
+    </>
+  ),
+  project: {
+    link: 'https://cubic-store.tebex.io',
+    icon: <StoreIcon />,
+  },
+  chat: {
+    link: 'https://discord.gg/VbDEEWBvJB',
+  },
+  footer: {
+    text: 'Cubic Store',
+  },
+  head: (
+    <>
+      <link
+        href="https://fonts.googleapis.com/css2?family=Fredoka+One&display=swap"
+        rel="stylesheet"
+      />
+    </>
+  ),
+  editLink: {
+    component: null,
+  },
+  feedback: {
+    content: null,
+  },
+};
+
+export default config;
